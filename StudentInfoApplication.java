8d230cf102fbdce9fc96cc357b088a7ad2c47904
Nagendrababu11 <111265888+Nagendrababu11@users.noreply.github.com>
2025-06-04 20:43:21 +0530

    Create StudentInfoApplication.java

---
+++ StudentInfoApplication.java
@@ -0,0 +1,10 @@
+package com.example.studentapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class StudentInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentInfoApplication.class, args);
+    }
+}
